aafb4c6c2eccefa578d3cf8e3415018c4e9734e7
Steven Edwards <stevenedwards@MacBook-Pro.local>
2020-09-08 10:40:51 -0400

    one li color changed

--- dom_practice.js
+++ dom_practice.js
@@ -11,3 +11,7 @@ headerVariable.innerText = 'Welcome to Raleigh!!!';
 //4
 let classArray = document.querySelectorAll('.class');
 console.log(classArray);
+
+//5
+let oneLi = document.querySelector('li');
+oneLi.style.color = '#FA8072';
